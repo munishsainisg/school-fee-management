79e1e3cae3bbde67e3c4abd77b2c1f818f27fe4b
munishsainisg <munishsainisg@gmail.com>
2026-03-13 22:20:28 -0400

    Initial Setup for School Fees Management Project

---
+++ src/main/java/com/school/management/studentfees/StudentFeesApplication.java
@@ -0,0 +1,13 @@
+package com.school.management.studentfees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentFeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentFeesApplication.class, args);
+	}
+
+}
